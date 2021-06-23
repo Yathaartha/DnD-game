3b8ca73f6aefcbb332916ba0ed3b29a868bd8ff5
Yathaartha <maharjanyathaartha017@gmail.com>
2021-06-23 20:05:14 +0545

    added different story for every choice

--- js/choices.js
+++ js/choices.js
@@ -1,22 +1,26 @@
 // the choices are listed here as an array with objects
 const choices = [
   {
+    story: "story",
     a: "Go With A",
     b: "Go With B",
     c: "Go With C",
   },
   [
     {
+      story: "story a",
       a: "aA",
       b: "aB",
       c: "aC",
     },
     {
+      story: "story b",
       a: "bA",
       b: "bB",
       c: "bC",
     },
     {
+      story: "story c",
       a: "cA",
       b: "cB",
       c: "cC",
@@ -24,16 +28,19 @@ const choices = [
   ],
   [
     {
+      story: "story a",
       a: "aA",
       b: "aB",
       c: "aC",
     },
     {
+      story: "story b",
       a: "bA",
       b: "bB",
       c: "bC",
     },
     {
+      story: "story c",
       a: "cA",
       b: "cB",
       c: "cC",
@@ -41,16 +48,19 @@ const choices = [
   ],
   [
     {
+      story: "story a",
       a: "aA",
       b: "aB",
       c: "aC",
     },
     {
+      story: "story b",
       a: "bA",
       b: "bB",
       c: "bC",
     },
     {
+      story: "story c",
       a: "cA",
       b: "cB",
       c: "cC",
@@ -58,26 +68,59 @@ const choices = [
   ],
   [
     {
+      story: "story a",
       a: "aA",
       b: "aB",
       c: "aC",
     },
     {
+      story: "story b",
       a: "bA",
       b: "bB",
       c: "bC",
     },
     {
+      story: "story c",
       a: "cA",
       b: "cB",
       c: "cC",
     },
+  ],
+  [
     {
+      story: "story a",
+      a: "aA",
+      b: "aB",
+      c: "aC",
+    },
+    {
+      story: "story b",
+      a: "bA",
+      b: "bB",
+      c: "bC",
+    },
+    {
+      story: "story c",
       a: "cA",
       b: "cB",
       c: "cC",
     },
+  ],
+  [
     {
+      story: "story a",
+      a: "aA",
+      b: "aB",
+      c: "aC",
+    },
+    {
+      story: "story b",
+      a: "bA",
+      b: "bB",
+      c: "bC",
+    },
+    {
+      story: "story c",
       a: "cA",
       b: "cB",
       c: "cC",
@@ -86,7 +129,8 @@ const choices = [
 ];
 
 // constants for DOM elements
-const content = document.querySelector(".cards");
+const content = document.querySelector("#choices");
+const ending = document.querySelector("#end");
 
 const playerChoices = document.querySelectorAll(".card");
 
@@ -94,7 +138,10 @@ const choiceA = document.getElementById("A");
 const choiceB = document.getElementById("B");
 const choiceC = document.getElementById("C");
 
+const story = document.querySelector(".lead");
+
 const selectBtn = document.getElementById("select");
+const restartBtn = document.getElementById("restart");
 
 // player choice is stored here
 let selectedChoice = {
@@ -107,10 +154,21 @@ let level = 0;
 // Event listener for select Button
 selectBtn.addEventListener("click", () => {
   if (selectedChoice.id != null && selectedChoice.lvl != null) {
+    level++;
     renderNextChoices(selectedChoice.id, selectedChoice.lvl);
   } else {
     alert("No choice selected");
   }
+});
+
+// Event listener for restart button
+restartBtn.addEventListener("click", () => {
+  content.style.display = "flex";
+  ending.style.display = "none";
+  init();
+  story.style.display = "initial"; //story displayed
+  selectBtn.style.display = "initial"; //Button displayed
+  restartBtn.style.display = "none"; //restart button not displayed
 });
 
 // event handler for choices
@@ -120,24 +178,21 @@ playerChoices.forEach((playerChoice) => {
     switch (choiceTag.textContent) {
       // Case A
       case "A":
-        level++; //increases the depth of the story
         selectChoice(playerChoice); //stores selection
         selectedChoice.id = 0; //choice A is denoted by 0 for the array
-        selectedChoice.lvl = level;
+        selectedChoice.lvl = level + 1;
         break;
       // Case B
       case "B":
-        level++;
         selectChoice(playerChoice);
         selectedChoice.id = 1;
-        selectedChoice.lvl = level;
+        selectedChoice.lvl = level + 1;
         break;
       // Case C
       case "C":
-        level++;
         selectChoice(playerChoice);
         selectedChoice.id = 2;
-        selectedChoice.lvl = level;
+        selectedChoice.lvl = level + 1;
         break;
       default:
         break;
@@ -147,6 +202,8 @@ playerChoices.forEach((playerChoice) => {
 
 // Initializes the values for the choices
 init = () => {
+  level = 0;
+  story.textContent = choices[0].story;
   choiceA.textContent = choices[0].a;
   choiceB.textContent = choices[0].b;
   choiceC.textContent = choices[0].c;
@@ -171,19 +228,17 @@ resetSelection = () => {
 renderNextChoices = (choice, level) => {
   resetSelection();
   if (choices.length > level) {
+    story.textContent = choices[level][choice].story;
     choiceA.textContent = choices[level][choice].a;
     choiceB.textContent = choices[level][choice].b;
     choiceC.textContent = choices[level][choice].c;
   } else {
     // if there are no more choices left
-    content.innerHTML = `<div class="card"  style="margin: 2rem auto">
-    <div class="content">
-
-      <div class="description" id="End">
-        The End
-      </div>
-    </div>
-  </div>`;
+    content.style.display = "none";
+    ending.style.display = "initial";
+    story.style.display = "none"; //story not displayed
+    selectBtn.style.display = "none"; //Button not displayed at end
+    restartBtn.style.display = "initial"; //restart button displayed
   }
 };
 
